d6c55688193552e7e4d8ce588fbb19832d322f15
邓思琴 <dengsiqin_develop@163.com>
2016-03-16 00:01:30 +0800

    tabBar 自定义设置

---
+++ MyYuWei/Home/Daily/Controller/DailyViewController.h
@@ -0,0 +1,13 @@
+//
+//  DailyViewController.h
+//  MyYuWei
+//
+//  Created by 邓思琴 on 16/3/15.
+//  Copyright © 2016年 邓思琴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyViewController : UIViewController
+
+@end
--- MyYuWei/Home/MainTabBarController.m
+++ MyYuWei/Home/MainTabBarController.m
@@ -7,8 +7,21 @@
 //
 
 #import "MainTabBarController.h"
+#import "CommendViewController.h"
+#import "CityViewController.h"
+#import "OneTimeViewController.h"
+#import "DailyViewController.h"
+#import "MineViewController.h"
 
-@interface MainTabBarController ()
+
+#define KScreenWidth [UIScreen mainScreen].bounds.size.width
+#define KScreenHeigth [UIScreen mainScreen].bounds.size.heigth
+
+#define KTabBarHeight 49
+
+@interface MainTabBarController (){
+    UIImageView * _bjImageView;
+}
 
 @end
 
@@ -20,7 +33,126 @@
     
     self.view.backgroundColor = [UIColor grayColor];
     
+    //创建子控制器
+    [self _creatViewControllers];
+    
+    //自定义标签栏
+    [self _creatTabBar];
+    
 }
+
+
+-(void)_creatViewControllers{
+    
+    //创建5个视图控制器
+    CommendViewController * commend = [[CommendViewController alloc]init];
+    CityViewController * city = [[CityViewController alloc]init];
+    OneTimeViewController * oneTime = [[OneTimeViewController alloc]init];
+    DailyViewController * daily = [[DailyViewController alloc]init];
+    MineViewController * mine = [[MineViewController alloc]init];
+    
+    
+    //添加到数组
+    
+    NSArray * viewControllers = @[commend,city,oneTime,daily,mine];
+    
+    //遍历视图控制器，给每一个视图控制器都添加一个导航控制器
+    //把导航控制器对象，添加到数组
+    
+//    NSMutableArray * navis = [[NSMutableArray alloc]initWithCapacity:viewControllers.count];
+//    for (UIViewController * viewC in viewControllers) {
+//        
+//        
+//        
+//        
+//        
+//        
+//    }
+//    
+//    
+    
+    
+    
+}
+
+//自定义标签栏
+
+-(void)_creatTabBar{
+    
+    //隐藏系统自带的标签项
+
+    for (UIView * view in self.tabBar.subviews) {
+        [view removeFromSuperview];
+    }
+    
+    CGFloat width = KScreenWidth / self.viewControllers.count;
+    NSArray * titles = @[@"推荐",@"全球",@"一刻",@"食记",@"我的"];
+    NSArray * images = @[@"tab_recommend",
+                         @"tab_find",
+                         @"tab_onetime",
+                         @"tab_daily",
+                         @"tab_mine"];
+    //创建ImageView,设置tabBar的背景图片
+    _bjImageView = [[UIImageView alloc]initWithFrame:self.tabBar.bounds];
+    _bjImageView.backgroundColor = [UIColor grayColor];
+    [self.tabBar addSubview:_bjImageView];
+    
+    for (int i = 0; i < titles.count; i ++) {
+        
+        UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.tag = 1000 + i ;
+        button.frame = CGRectMake(i * KScreenWidth / 5, 0, KScreenWidth / 5, KTabBarHeight);
+        [_bjImageView addSubview:button];
+        
+        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(20, 5, KScreenWidth / 5 -40 , KTabBarHeight - 28)];
+        imageView.image = [UIImage imageNamed:images[i]];
+        imageView.tag = 1100;
+        [button addSubview:imageView];
+        
+        UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(0, KTabBarHeight - 15, KScreenWidth / 5, 14)];
+        label.textColor = [UIColor whiteColor];
+        label.text = titles[i];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.font = [UIFont systemFontOfSize:12];
+        label.tag = 100;
+        [button addSubview:label];
+        
+        
+        
+        
+        
+        
+        
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+                                
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
