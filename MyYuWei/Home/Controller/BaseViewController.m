0b34dfd9033abc87f75cb98677261908dd54da2c
邓思琴 <dengsiqin_develop@163.com>
2016-03-17 11:46:22 +0800

    tabBar的点击事件完成

---
+++ MyYuWei/Home/Controller/BaseViewController.m
@@ -0,0 +1,40 @@
+//
+//  BaseViewController.m
+//  MyYuWei
+//
+//  Created by 邓思琴 on 16/3/17.
+//  Copyright © 2016年 邓思琴. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+#warning 创建BaseViewController 将其他控制器继承于base 以便重用相同布局或者功能
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- MyYuWei/Home/MainTabBarController.m
+++ MyYuWei/Home/MainTabBarController.m
@@ -116,9 +116,13 @@
         UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
         button.tag = 1000 + i ;
         button.frame = CGRectMake(i * KScreenWidth / 5, 0, KScreenWidth / 5, KTabBarHeight);
-        [_bjImageView addSubview:button];
-        
+
         UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(20, 5, KScreenWidth / 5 -40 , KTabBarHeight - 28)];
+        
+#warning 设置imageView的内容模式 自适应 防止被拉伸 因为你的imageView尺寸比例与图标大小不符合
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        
+        
         imageView.image = [UIImage imageNamed:images[i]];
         imageView.tag = 1100;
         [button addSubview:imageView];
@@ -133,6 +137,25 @@
         
         
         
+#warning 方案二 不绘制新的imageView和label 直接设置button的图片和文字 然后修改图片和文字的偏移量 默认图片在左边 文字在右边 仅供学习偏移量
+        /*
+        [button setImage:[UIImage imageNamed:images[i]] forState:UIControlStateNormal];
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        button.titleLabel.font = [UIFont systemFontOfSize:12];
+        button.backgroundColor = [UIColor blueColor];
+        button.layer.borderColor = [UIColor blackColor].CGColor;//layer层的边框颜色
+        button.layer.borderWidth = 1;//layer层的边框宽
+        //设置button图片的偏移 //类似这样的坐标button.frame.size.width 可以通过第三方类库简单调用 自己查找下载导入 或者自行封装 实现 类似view.width = view.frame.size.width
+        //计算坐标偏移量
+        CGFloat leftEdge = (button.frame.size.width - button.imageView.frame.size.width)/2 ;
+
+        [button setImageEdgeInsets:UIEdgeInsetsMake(- button.titleLabel.frame.size.height, leftEdge, 0,0)];
+        button.titleLabel.backgroundColor = [UIColor redColor];
+        button.imageView.backgroundColor = [UIColor purpleColor];
+        [button setTitleEdgeInsets:UIEdgeInsetsMake(button.imageView.frame.size.height + 2, -(button.frame.size.width - button.titleLabel.frame.size.width)/2, 0, 0)];
+        //UIEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right) 上左下右四个方向的偏移量
+        
+        */
         //tabBar的点击事件
         [button addTarget:self action:@selector(clickTabBarButton:) forControlEvents:UIControlEventTouchUpInside];
         
@@ -145,7 +168,7 @@
             label.textColor = [UIColor orangeColor];
         }
         
-        
+        [self.tabBar addSubview:button];
     }
                                 
    
@@ -169,6 +192,38 @@
                          @"tab_daily",
                          @"tab_mine"];
     
+    
+    if (sender.tag - 1000 < 5) {
+        self.selectedIndex = sender.tag - 1000;
+        UIImageView * imageView = (UIImageView *)[sender viewWithTag:1100];
+        UILabel * label = (UILabel *)[sender viewWithTag:1101];
+        label.textColor = [UIColor orangeColor];
+        imageView.image = [UIImage imageNamed:imagesOranges[sender.tag - 1000]];
+        
+        for (int i = 0; i < 5; i ++) {
+            
+            if (sender.tag - 1000 == i) {
+                
+            self.selectedIndex = sender.tag - 1000;
+
+            UIImageView * imageView = (UIImageView *)[sender viewWithTag:1100];
+            UILabel * label = (UILabel *)[sender viewWithTag:1101];
+            label.textColor = [UIColor orangeColor];
+            imageView.image = [UIImage imageNamed:imagesOranges[sender.tag - 1000]];
+
+            
+            }else{
+                UIButton * button = (UIButton * )[self.tabBar viewWithTag:1000 + i];
+                UIImageView * imageView = (UIImageView * )[button viewWithTag:1100];
+                UILabel * label = (UILabel *)[button viewWithTag:1101];
+                label.textColor = [UIColor whiteColor];
+                imageView.image = [UIImage imageNamed:images[i]];
+                
+            }
+        
+    }
+    
+    }
     
   
 }
